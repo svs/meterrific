a2ff8ea65ce0ca1c4d1fd14cea7bb140615bd5d9
svs <svs@svs.io>
2013-03-02 11:36:06 +0530

    Scheme validation working

---
+++ lib/domain/scheme.rb
@@ -0,0 +1,28 @@
+require 'active_support/all'
+require_relative 'km_scheme'
+require_relative 'wait_scheme'
+class Scheme
+
+  # preferred scheme format is
+  # { :kms => 21 or  { 10 => 21, :then => 15 },
+  #   :wait => {:per_minute => 3, :wait_speed => 15}
+  #   
+  
+
+  def initialize(scheme)
+    @km_scheme = KmScheme.new(scheme[:kms])
+    @wait_scheme = WaitScheme.new(scheme[:wait])
+  end
+
+  def valid?
+    km_scheme.valid? && wait_scheme.valid?
+  end
+
+
+  private
+
+  attr_reader :km_scheme, :wait_scheme
+
+
+  
+end
--- spec/domain/scheme_spec.rb
+++ spec/domain/scheme_spec.rb
@@ -4,13 +4,13 @@ describe Scheme do
 
   describe "valid scheme" do
     context "flat km rate" do
-      let(:valid_params) { {:kms => 21, :wait_per_min => 3, :wait_speed => 15} }
+      let(:valid_params) { {:kms => 21, :wait => {:per_minute => 3, :wait_speed => 15}} }
       subject { Scheme.new(valid_params) }
       it { should be_valid }
     end
 
     context "stepped km rate" do
-      let(:valid_params) { {:kms => { "10" => 25, "then" => 20}, :wait_per_min => 3, :wait_speed => 15} }
+      let(:valid_params) { {:kms => { "10" => 25, "then" => 20}, :wait => {:per_minute => 3, :wait_speed => 15}} }
       subject { Scheme.new(valid_params) }
       it { should be_valid }
     end
