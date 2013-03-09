d2e07c87119cd322064a91dad53e4d110e37941e
svs <svs@svs.io>
2013-03-10 01:11:31 +0530

    mismatched parenthesis!

--- webapp/cab_meters.rb
+++ webapp/cab_meters.rb
@@ -10,7 +10,7 @@ module CabMetersController
 
     def post
       @api_key = ApiKey.first(:api_key => params[:api_key])
-      return fail_with(JSON.dump({:errors => "invalid API key"}, 401)) unless @api_key
+      return fail_with(JSON.dump({:errors => "invalid API key"}), 401) unless @api_key
       @cab_meter = CabMeter.new(params[:cab_meter].merge(:api_key => @api_key))
       @cab_meter.save ? succeed_with(JSON.dump(@cab_meter.attributes)) : fail_with(JSON.dump(@cab_meter.errors.to_hash), 422)
     end
